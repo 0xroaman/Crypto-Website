c028bb6fdf63731ae4d91e28279825c329c7c4e4
0xroaman <luiscordes0102@gmail.com>
2025-03-14 11:30:15 +0900

    initial commit

---
+++ app/js/popup.js
@@ -0,0 +1,7 @@
+const ethereumButton = document.querySelector('.enableEthereumButton'); 
+const showAccount = document.querySelector('#app-content'); 
+ethereumButton.addEventListener('click', () => { getAccount(); }); 
+async function getAccount() { 
+    const accounts = await ethereum.request({ method: 'eth_requestAccounts' }); 
+    const account = accounts[0]; showAccount.innerHTML = account; 
+}
