45baa0b16b057971c37ade85c29bfaca8f94abd3
0xroaman <luiscordes0102@gmail.com>
2025-03-18 11:39:08 +0900

    update index.js

--- app/js/index.js
+++ app/js/index.js
@@ -4,3 +4,10 @@ const jwt = require('jsonwebtoken');
 const Table = require('../pokergame/Table');
 const Player = require('../pokergame/Player');
 
+import 'core-js/es/map';
+import 'core-js/es/set';
+import 'raf/polyfill';
+
+const rootElement = document.getElementById('root');
+const cookieBannerRoot = document.getElementById('cookie-banner');
+const loadingScreen = document.getElementById('loading-screen');
